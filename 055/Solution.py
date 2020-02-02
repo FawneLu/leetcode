66221db65848e62d6e7ce8e56fb9c543768ffd4c
FawneLu <tracylu1996@gmail.com>
2020-02-01 22:21:05 -0500

    2020-02-01

---
+++ 055/Solution.py
@@ -0,0 +1,11 @@
+```python
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reach=0
+        for i, num in enumerate(nums):
+            if i>reach:
+                return False
+            reach=max(reach,i+num)
+        
+        return True
+```
