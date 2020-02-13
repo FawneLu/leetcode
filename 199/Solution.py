f04d09477a8481b12899e98030aa4c1c9fbdc381
FawneLu <tracylu1996@gmail.com>
2020-02-12 21:34:02 -0500

    2020-02-12

---
+++ 199/Solution.py
@@ -0,0 +1,28 @@
+```python
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return None
+        
+        res=[]
+        queue=deque()
+        queue.append(root)
+        while queue:
+            res.append(queue[-1].val)
+            for i in range(len(queue)):
+                node=queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return res
+```
