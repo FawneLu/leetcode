969b5192935ac83a83a92ce4935163aef48fae77
FawneLu <tracylu1996@gmail.com>
2020-02-17 21:13:05 -0500

    2020-02-17

--- 282/Solution.py
+++ 282/Solution.py
@@ -1,6 +1,32 @@
 ```python
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
+        def dfs(l,r,exp,cur,last,res):
+            if l==r:
+                if cur==target:
+                    res.append(exp)
+                return
+            
+            for i in range(l+1,r+1):
+                if i==l+1 or (i>l+1 and num[l]!='0'):
+                    s,x=num[l:i],int(num[l:i])
+                    
+                    if last==None:
+                        dfs(i,r,s,x,x,res)
+                        
+                    else:
+                        dfs(i,r,exp+'+'+s,cur+x,x,res)
+                        dfs(i,r,exp+'-'+s,cur-x,-x,res)
+                        dfs(i,r,exp+'*'+s,cur-last+last*x,last*x,res)
+        
+        res=[]
+        dfs(0,len(num),"",0,None,res)
+        return res
+```
+
+```python
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
         res=[]
         string=" "*(len(num)*2)
         self.dfs(num,target,0,string,0,0,0,res)
