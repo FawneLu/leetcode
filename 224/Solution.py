61fa7d1baef3db312deff5bc2878e6b807987b7e
FawneLu <tracylu1996@gmail.com>
2020-04-01 20:51:36 -0400

    2020-04-01

--- 224/Solution.py
+++ 224/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def calculate(self, s: str) -> int:
+    def calculate1(self, s: str) -> int:
         res, num, sign, stack=0, 0, 1, [1]
         
         for i in s + "+":
@@ -16,5 +16,37 @@ class Solution:
                 res += num*sign*stack[-1]
                 num=0
                 stack.pop()
+        
+        return res
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, num, sign = 0, 0, 1
+        stack=[]
+        
+        for i in s:
+            
+            if i.isdigit():
+                num = num*10+int(i)
+            
+            elif i in "+-":
+                res += num*sign
+                num = 0
+                sign = 1 if i == "+" else -1
+            
+            elif i == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign=1
+            
+            elif i == ")":
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num=0
+        
+        res += num*sign
         
         return res
