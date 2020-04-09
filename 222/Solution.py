3ca40dbb8eab1da5e863e3f377c138982c6e0f46
FawneLu <tracylu1996@gmail.com>
2020-04-09 01:01:48 -0400

    2020-04-09

--- 222/Solution.py
+++ 222/Solution.py
@@ -1,4 +1,3 @@
-```python
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -8,5 +7,43 @@
 
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        def getheight(root):
+            height=0
+            while root:
+                height+=1
+                root=root.left
+            return height
+        
+        nodes=0
+        
+        left_height=getheight(root.left)
+        right_height=getheight(root.right)
+        
+        if left_height==right_height:
+            nodes+=2**left_height+self.countNodes(root.right)
+        else:
+            nodes+=2**right_height+self.countNodes(root.left)
+            
+        return nodes
+    
+
+
+
+    def countNodes2(self, root: TreeNode) -> int:
         return 1+self.countNodes(root.left)+self.countNodes(root.right) if root else 0
-```
+
+
+    def countNodes3(self, root: TreeNode) -> int:
+        
+        def helper(root,res):
+            if not root:
+                return 0
+            else:
+                res=res+helper(root.left,res)+helper(root.right,res)
+            
+            return res+1
+        
+        return helper(root,0)
--- 222/note.md
+++ 222/note.md
@@ -1,3 +1,17 @@
 ### 计算一个完全二叉树有多少个节点
 - 用linear的方法
 忽略完全二叉树的性质，就看一个树一共有多少个节点。
+- 利用完全二叉树的性质做  
+完全二叉树Complete Binary Tree是指除了最后一行以外，每一行都是满的。如果最后一行存在节点，那么这些节点满足从左到右排列。  
+满二叉树Full Binary Tree是指所有的节点的子节点要么为空要么有两个子节点。而满二叉树的节点总数是 2 ^ h - 1，这里h是满二叉树的高度。  
+知道了以上性质，我们就可以进行解题  
+先构造一个getHeight方法， 用来求出二叉树的高度。这里我们只用求从根节点到最左端节点的长度。
+求出根节点左子树高度leftHeight和根节点右子树高度rightHeight
+假如两者相等，那么说明左子树是满二叉树，而右子树可能是完全二叉树。  
+我们可以返回  2 ^ leftHeight - 1 + 1  + countNodes(root.right)  
+这里+1是因为把根节点也算进去，化简一下就是 1 << leftHeight + countNodes(root.right)，返回结果  
+否则两者不等，说明左子树是完全二叉树，右子树是满二叉树
+我们可以返回 2^ rightHeight - 1 + 1 + countNodeS(root.left)  
+化简以后得到 1 << rightHeight + countNodes(root.left)，返回结果  
+这里getHeight()方法的时间复杂度是O(logn)， countNodes()方法的时间复杂度也是O(logn)，所以总的时间复杂度是O(logn * logn)
+空间复杂度是递归栈的深度，是O(logn)  
