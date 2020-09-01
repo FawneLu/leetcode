56644fea2475b102f0f031a04a4e77452ca4849a
FawneLu <tracylu1996@gmail.com>
2020-09-01 17:05:43 -0400

    2020-09-01

--- 315/Solution.py
+++ 315/Solution.py
@@ -14,3 +14,29 @@ class Solution:
 
         return ans
 ```
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        self.smaller = [0]*len(nums)
+        
+        def mergesort(enum):
+            half = len(enum) // 2
+            
+            if half:
+                left, right = mergesort(enum[:half]), mergesort(enum[half:])
+                m, n = len(left), len(right)
+                i, j = 0, 0
+                while j < n or i < m:
+                    if j == n or i < m and left[i][1] <= right[j][1]:
+                        enum[i+j] = left[i]
+                        self.smaller[left[i][0]] += j
+                        i += 1
+                    else:
+                        enum[i+j] = right[j]
+                        j += 1
+                
+            return enum
+        
+        
+        mergesort(list(enumerate(nums)))
+        return self.smaller
--- 315/note.md
+++ 315/note.md
@@ -1,2 +1,6 @@
 ### 二分查找
 我们用binary search来查找一个list里放置一个数x的index，使得，list[:i]< x, list[i+1:]>x, 这个位置就是我们每次求得的结果。
+
+###用mergesort的思路
+这个方法真的是fucking genius。  
+enumerate数组之后，不断二分，然后把enumerate的左右的数组进行排序。 不断把右边往左边插，看右边的前面的enum插了几个就证明了有几个数在后面比它小
