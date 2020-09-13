3597c2683568693e488edfffb6ced51ad3897eaf
FawneLu <tracylu1996@gmail.com>
2020-09-13 01:14:44 -0400

    2020-09-13

--- 080/Solution.py
+++ 080/Solution.py
@@ -26,3 +26,18 @@ class Solution:
             i += 1
         
         return len(nums)
+
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return None
+        if len(nums) == 2:
+            return 2
+        
+        slow = 2
+        for fast in range(2, len(nums)):
+            if nums[fast] == nums[slow - 2]:
+                continue
+            nums[slow] = nums[fast]
+            slow += 1
+        return slow
