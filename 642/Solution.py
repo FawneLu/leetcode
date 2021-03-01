7e73bfa69951486b72b0342902344bd6fc1d969e
FawneLu <tracylu1996@gmail.com>
2021-03-01 00:20:48 -0500

    2021-02-28

---
+++ 642/Solution.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.sentences = set()
+    
+        
+class AutocompleteSystem:
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.buffer = ""
+        self.stimes = collections.defaultdict(int)
+        self.trie = TrieNode()
+        
+        for s, t in zip(sentences,times):
+            self.stimes[s] = t
+            self.addSentence(s)
+        self.tnode = self.trie
+        
+        
+
+    def input(self, c: str) -> List[str]:
+        ans = []
+        if c != "#":
+            self.buffer += c
+            if self.tnode: self.tnode=self.tnode.children.get(c)
+            if self.tnode: ans = sorted(self.tnode.sentences, key = lambda x :(-self.stimes[x],x))[:3]
+        
+        else:
+            self.stimes[self.buffer] += 1
+            self.addSentence(self.buffer)
+            self.buffer = ""
+            self.tnode = self.trie
+        
+        return ans
+    
+    def addSentence(self,sentence):
+        node = self.trie
+        for letter in sentence:
+            child = node.children.get(letter)
+            if not child:
+                child = TrieNode()
+                node.children[letter] = child
+            
+            node = child
+            child.sentences.add(sentence)
